054404ce88d4473be2473381cab89d64e4de2331
David Anderson <david@pibcak.net>
2020-07-04 14:50:43 +0100

    Reduced the amount of platforms to build on

--- build/run-build.sh
+++ build/run-build.sh
@@ -2,17 +2,21 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  echo "Starting Branch / PR Build"
   docker buildx build \
     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6 \
     .
   exit $?
+else
+  echo "Starting Master Build"
+  echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /dev/null
+  TAG="${TRAVIS_TAG:-latest}"
+  docker buildx build \
+       --progress plain \
+      --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+      -t $DOCKER_REPO:$TAG \
+      --push \
+      .
+  exit $?
 fi
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /dev/null
-TAG="${TRAVIS_TAG:-latest}"
-docker buildx build \
-     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
-    -t $DOCKER_REPO:$TAG \
-    --push \
-    .
